7ce52bc9fedc382b9e96de6b12eb777ff9394b60
semisenioritis <atharvadhamankar43@gmail.com>
2023-03-07 03:00:17 +0530

    This is my final commit
    Here i finally begin assembling  my website and pushing it from my local host to github

--- javascript/wallpapertester.js
+++ javascript/wallpapertester.js
@@ -1,7 +1,8 @@
 var tochange = document.getElementsByTagName('body');
 var counter= 1
+var headerel= document.getElementById("image_mask");
 
-var pathh= "/backgrounds/ghibli/maybe/tot/ghibli ("
+var pathh= "/backgrounds/ghibli/ghiblifull/ghibli ("
 var exttt= ").jpg"
 document.getElementById('slideRight').onclick = function () {
     counter= counter+1
@@ -11,8 +12,10 @@ document.getElementById('slideRight').onclick = function () {
     finpath= urlstart+finpath
     finpath=finpath+urlend
     document.body.style.backgroundImage =finpath
-    document.getElementsByClassName("my-photo")[0].innerHTML= String(counter)
+    document.getElementsByClassName("counter_holder")[0].innerHTML= String(counter)
+    headerel.style.backgroundImage =finpath
     console.log(finpath)
+
 };
 
 document.getElementById('slideLeft').onclick = function () {
@@ -23,6 +26,35 @@ document.getElementById('slideLeft').onclick = function () {
     finpath= urlstart+finpath
     finpath=finpath+urlend
     document.body.style.backgroundImage =finpath
-    document.getElementsByClassName("my-photo")[0].innerHTML= String(counter)
+    document.getElementsByClassName("counter_holder")[0].innerHTML= String(counter)
+    headerel.style.backgroundImage =finpath
     console.log(finpath)
     };
+
+
+    document.addEventListener('keydown', function(event) {
+        if(event.keyCode == 39) {
+            counter= counter+1
+            let finpath= pathh+ String(counter)+exttt
+            urlstart= 'url("'
+            urlend= '")'
+            finpath= urlstart+finpath
+            finpath=finpath+urlend
+            document.body.style.backgroundImage =finpath
+            document.getElementsByClassName("counter_holder")[0].innerHTML= String(counter)
+            headerel.style.backgroundImage =finpath
+            console.log(finpath)
+        }
+        else if(event.keyCode == 37) {
+            counter= counter-1
+            let finpath= pathh+ String(counter)+exttt
+            urlstart= 'url("'
+            urlend= '")'
+            finpath= urlstart+finpath
+            finpath=finpath+urlend
+            document.body.style.backgroundImage =finpath
+            document.getElementsByClassName("counter_holder")[0].innerHTML= String(counter)
+            headerel.style.backgroundImage =finpath
+            console.log(finpath)
+        }
+    });
